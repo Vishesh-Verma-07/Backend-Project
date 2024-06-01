4f180530c6af69aecde9150e3db0e6367eb932ac
Vishesh Verma <visheshverma250703@gmail.com>
2024-06-01 11:30:49 +0530

    name changes

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
        console.log(`\n Mongodb Connected !! DB Host: ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log("Mongodb connection Failed: ",error)
+        console.log("MongoDb connection Failed: ",error)
         process.exit(1);
     }
 }
